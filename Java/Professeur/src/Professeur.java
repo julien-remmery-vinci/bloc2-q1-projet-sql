b959fa7c7ca95de72e0a61254f9bc814e5e0b625
julien-remmery-vinci <julien.remmery@student.vinci.be>
2023-12-08 12:51:43 +0100

    update

--- Java/Professeur/src/Professeur.java
+++ Java/Professeur/src/Professeur.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 
 public class Professeur {
 //    static String url= "jdbc:postgresql://localhost:5432/postgres";
-    //    static String url= "jdbc:postgresql://localhost:5432/postgres";
-    static String url= "jdbc:postgresql://172.24.2.6:5432/dbnadirahdid";
+        static String url= "jdbc:postgresql://localhost:5432/postgres";
+//    static String url= "jdbc:postgresql://172.24.2.6:5432/dbnadirahdid";
 
     static Connection conn=null;
     static Scanner scanner = new Scanner(System.in);
@@ -22,8 +22,8 @@ public class Professeur {
     static{
         try {
             try {
-//               JULIEN conn = DriverManager.getConnection(url,"postgres","fvG78Dy%");
-                conn = DriverManager.getConnection(url,"nadirahdid","K51Y3WAJP");
+                conn = DriverManager.getConnection(url,"postgres","fvG78Dy%");
+//                conn = DriverManager.getConnection(url,"nadirahdid","K51Y3WAJP");
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
